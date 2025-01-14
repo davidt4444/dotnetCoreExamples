7835ad6d39e434d2c9779c4b87b07c2123888f02
davidt4444 <david@davidthigpen.com>
2025-01-13 19:54:52 -0600

    Add in changes to scheme

--- basic_content_service/BCS.Api/obj/Debug/net9.0/BCS.Api.AssemblyInfo.cs
+++ basic_content_service/BCS.Api/obj/Debug/net9.0/BCS.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BCS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f360e93de880c42a741ed1cc4eb42ef295538164")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0c09ee54963b7f0699103c009800b309218320")]
 [assembly: System.Reflection.AssemblyProductAttribute("BCS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BCS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
